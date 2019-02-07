9f9e31807e356785083777f963b0a596e736fafc
rhodolfosantana <31549809+rhodolfosantana@users.noreply.github.com>
2019-02-07 12:12:19 -0200

    Criando template, footer e redimensionando background

---
+++ footer.php
@@ -0,0 +1,73 @@
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<style type="text/css">
+  
+
+
+.footer-classic a, .footer-classic a:focus, .footer-classic a:active {
+    color: black;
+}
+.nav-list li {
+    padding-top: 5px;
+    padding-bottom: 5px;
+}
+
+.nav-list li a:hover:before {
+    margin-left: 0;
+    opacity: 1;
+    visibility: visible;
+}
+
+ul, ol {
+    list-style: none;
+}
+
+.social-inner {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 15px;
+    text-transform: uppercase;
+}
+
+  
+</style> 
+
+<!------ Include the above in your HEAD tag ---------->
+
+<footer class="section footer-classic context-dark bg-image" style="background:grey;">
+        <div class="container">
+          <div class="row row-30">
+            <div class="col-md-4 col-xl-5">
+              <div class="pr-xl-4"><a class="brand" href="index.html"><img class="brand-logo-light"  ></a>
+                <p></p>
+                <!-- Rights-->
+                
+              </div>
+            </div>
+            <div class="col-md-4">
+              <h5>Contacts</h5>
+              
+              <dl class="contact-list">
+                <dt>email:</dt>
+                <dd><a href="mailto:#"></a></dd>
+              </dl>
+              <dl class="contact-list">
+                
+              </dl>
+            </div>
+            <div class="col-md-4 col-xl-3">
+              <h5>Links</h5>
+              <ul class="nav-list">
+                <li><a href="#">About</a></li>
+                <li><a href="#">Projects</a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div class="row no-gutters social-container">
+          <div class="col"><a class="social-inner" href="#"><span class="icon mdi mdi-instagram"></span><span>instagram</span></a></div>
+          <div class="col"><a class="social-inner" href="#"><span class="icon mdi mdi-twitter"></span><span>twitter</span></a></div>
+        </div>
+      </footer>
---
+++ index.php
@@ -0,0 +1,155 @@
+
+<?php include 'header.php '?>
+<!DOCTYPE html>
+<html>
+<head>
+	<style type="text/css">
+		
+		body {
+  background: #eaeaea;
+  font-family: 'PT Mono', monospace;
+  text-align: center;
+}
+
+.container {
+  background: #ddd;
+  padding: 2rem 0;
+  width: 100%;
+}
+
+.title {
+	background-size: 100% 100%;
+  	background-image: url('img/png.jpeg');
+    background-attachment: fixed;
+  	height: 60vh;
+  	padding: 2rem 0;
+}
+
+h1 {
+  font-family: 'Bangers', cursive;
+  font-size: 5rem;
+  color: #fff;
+  text-shadow: 2px 2px 2px #333;
+}
+
+
+
+p {
+  margin-bottom: 1rem;
+}
+	</style>
+</head>
+<body>
+	<div class="container title">
+	</div>
+
+<div class="container">
+	<p>RECOMEÃ‡AR
+
+NÃ£o importa onde vocÃª parou,
+em que momento da vida vocÃª cansou,
+o que importa Ã© que sempre Ã© possÃ­vel
+e necessÃ¡rio "RecomeÃ§ar".
+
+RecomeÃ§ar Ã© dar uma nova
+chance a si mesmo.
+Ã‰ renovar as esperanÃ§as na vida
+e o mais importante:
+acreditar em vocÃª de novo.
+
+Sofreu muito nesse perÃ­odo?
+Foi aprendizado.
+
+Chorou muito?
+Foi limpeza da alma.
+
+Ficou com raiva das pessoas?
+Foi para perdoÃ¡-las um dia.
+
+Sentiu-se sÃ³ por diversas vezes?
+Ã‰ por que fechaste a porta atÃ© para os outros.
+
+Acreditou que tudo estava perdido?
+Era o inÃ­cio da tua melhora.
+
+Pois Ã©!
+Agora Ã© hora de iniciar,
+de pensar na luz,
+de encontrar prazer nas coisas simples de novo.
+
+Que tal um novo emprego?
+Uma nova profissÃ£o?
+Um corte de cabelo arrojado, diferente?
+Um novo curso,
+ou aquele velho desejo de aprender a pintar,
+desenhar,
+dominar o computador,
+ou qualquer outra coisa?
+
+Olha quanto desafio.
+Quanta coisa nova nesse mundÃ£o
+de meu Deus te esperando.
+
+TÃ¡ se sentindo sozinho?
+Besteira!
+Tem tanta gente que vocÃª afastou
+com o seu "perÃ­odo de isolamento",
+tem tanta gente esperando apenas um
+sorriso teu para "chegar" perto de vocÃª.
+
+Quando nos trancamos na tristeza nem
+nÃ³s mesmos nos suportamos.
+Ficamos horrÃ­veis.
+O mau humor vai comendo nosso fÃ­gado,
+atÃ© a boca ficar amarga.
+
+RecomeÃ§ar!
+Hoje Ã© um bom dia para comeÃ§ar
+novos desafios.
+
+Onde vocÃª quer chegar?
+Ir alto.
+Sonhe alto,
+queira o melhor do melhor,
+queira coisas boas para a vida.
+pensamentos assim trazem para nÃ³s
+aquilo que desejamos.
+
+Se pensarmos pequeno,
+coisas pequenas teremos.
+
+JÃ¡ se desejarmos fortemente o melhor
+e principalmente lutarmos pelo melhor,
+o melhor vai se instalar na nossa vida.
+
+E Ã© hoje o dia da Faxina Mental.
+
+Joga fora tudo que te prende ao passado,
+ao mundinho de coisas tristes,
+fotos,
+peÃ§as de roupa,
+papel de bala,
+ingressos de cinema,
+bilhetes de viagens,
+e toda aquela tranqueira que guardamos
+quando nos julgamos apaixonados.
+Jogue tudo fora.
+Mas, principalmente,
+esvazie seu coraÃ§Ã£o.
+Fique pronto para a vida,
+para um novo amor.
+
+Lembre-se somos apaixonÃ¡veis,
+somos sempre capazes de amar
+muitas e muitas vezes.
+Afinal de contas,
+nÃ³s somos o "Amor".</p>
+</div>
+<div class="container title">
+	</div>
+
+
+</body>
+</html>
+<?php include 'footer.php '?>
+
